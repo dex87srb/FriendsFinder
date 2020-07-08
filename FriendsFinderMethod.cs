bf0d1fbde50f7bce582e065d247c55074ec99c54
Danijel Tomic <dex87srb@gmail.com>
2020-07-08 21:51:27 +0200

    Separate class (method finder)

---
+++ FriendsFinderMethod.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SocialApp
+{
+    class FriendsFinder
+    {
+        string path = Path.GetFullPath(@"E:\dokumenti\Web programiranje kursevi i ostalo\C#\Projekti u toku\FriendsFinder\JSON database\dataFriends.json");
+
+        string input;
+        public FriendsFinder(string input)
+        {
+            this.input = input;
+        }
+
+       public void FriendsFinderMethod()
+        {
+            using StreamReader sr = new StreamReader(path);
+            string data = sr.ReadToEnd();
+
+            dynamic array = JsonConvert.DeserializeObject(data);
+
+            Dictionary<int, string> idsValuesPair = new Dictionary<int, string>();
+
+            foreach (var item in array)
+            {
+                JValue id = new JValue(item.id);
+                int idCast = (int)id;
+
+                JValue first = new JValue(item.firstName);
+                string firstCast = (string)first;
+
+                idsValuesPair.Add(idCast, firstCast);
+
+            }
+
+            WriteLine("Friends names are: ");
+
+            foreach (var pairCollection in idsValuesPair)
+            {
+                if (pairCollection.Value == input)
+                {
+                    foreach (var parseJSON in array)
+                    {
+                        JArray friends = new JArray(parseJSON.friends);
+
+                        for (int i = 0; i < friends.Count; i++)
+                        {
+                            if (pairCollection.Key == (int)friends[i])
+                            {
+                                JValue firstN = new JValue(parseJSON.firstName);
+                                string firstCast = (string)firstN;
+                                WriteLine(firstCast);
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+using System;
+
+namespace SocialApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {               
+                WriteLine("Insert friend name: ");
+                var input = ReadLine();
+                while (string.IsNullOrEmpty(input) || int.TryParse(input, out _))
+                {
+                    WriteLine("Please insert a name containing only letter: ");
+                    input = ReadLine();
+                }
+                WriteLine();
+
+                FriendsFinder obj = new FriendsFinder(input);
+
+                obj.FriendsFinderMethod();
+               
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.Message);
+            }
+          
+        }
+    }
+}
